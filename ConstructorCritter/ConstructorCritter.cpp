1b8e01f42c294ed39de0574bf050c08343bbb04a
Dmitry <Ivanovdmitry530@gmail.com>
2023-07-19 10:38:59 +0400

    Добавьте файлы проекта.

---
+++ ConstructorCritter/ConstructorCritter.cpp
@@ -0,0 +1,29 @@
+﻿// ConstructorCritter
+// Демонстрирует работу с конструкторами
+#include <iostream>
+using namespace std;
+
+class Critter 
+{
+    public:
+        int m_Hunger;
+        Critter(int hunger = 0); // прототип кинтруктора
+        void Greet();
+};
+Critter::Critter(int hunger) // определение конструктора
+{
+    cout << "A new critter has been born!" << endl;
+    m_Hunger = hunger;
+}
+void Critter::Greet()
+{
+    cout << "Hi, I'm a critter. My hunger level is " << m_Hunger << ".\n\n";
+}
+
+int main()
+{
+    Critter crit(7);
+    crit.Greet();
+    return 0;
+}
+
